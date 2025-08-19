a636acfc5ba1b4250dcf525118448b95a176ff80
Mathieu Latreille <matlat@yahoo.com>
2025-08-19 16:18:20 +0100

    Create Simple interest.sh

---
+++ Simple interest.sh
@@ -0,0 +1,1 @@
+Here is a script that calculates simple interest given principle, annual rate of interest and time period in years. It prompts users for inputs and output calculated simple interest. 
