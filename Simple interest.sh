dabc1e886ef3528f962422f8a7eb58cb9f8ee2dd
Mathieu Latreille <matlat@yahoo.com>
2025-08-19 16:20:06 +0100

    Update Simple interest.sh

--- Simple interest.sh
+++ Simple interest.sh
@@ -1,1 +1,9 @@
-Here is a script that calculates simple interest given principle, annual rate of interest and time period in years. It prompts users for inputs and output calculated simple interest. 
+# Here is a script that calculates simple interest given principle, annual rate of interest and time period in years. It prompts users for inputs and output calculated simple interest. 
+
+ #!/bin/bash
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Do not use this in production. Sample purpose only.
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <atlat@yahoo.com
